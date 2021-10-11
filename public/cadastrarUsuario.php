82766882a6b99581931ec01ca4d1764d3fbe4a80
Victor Ribeiro <v.leitteribeiro@gmail.com>
2021-10-11 02:11:28 -0300

    public/cadastrarUsuario:

--- public/cadastrarUsuario.php
+++ public/cadastrarUsuario.php
@@ -1,10 +1,4 @@
 <?php
 
-session_start();
 
-include_once("/xampp/htdocs/paranga-study/database/connection.php");
-$user = [];
-$query = "SELECT * FROM contato";
-$stmt = $conn->prepare($query);
-$stmt->execute();
-$user = $stmt->fetchAll(); 
+.alert-light{}
